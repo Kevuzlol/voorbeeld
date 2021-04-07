22e74a27b9aed1bdfc1e9ce38db66815cce9bee1
Kevuzlol <75366810+Kevuzlol@users.noreply.github.com>
2021-04-07 21:53:58 +0200

    Add files via upload

---
+++ voorbeeld/home.php
@@ -0,0 +1,73 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "voorbeeld";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+?>
+<!DOCTYPE html>
+<html>
+<title>voorbeeldtitle</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="styles/style.css">
+<body>
+
+<!-- Navigation -->
+<div class="w3-top">
+  <div class="w3-row w3-large w3-light-grey">
+    <div class="w3-col s3">
+      <a href="home.php" class="w3-button w3-block">Home</a>
+    </div>
+		<div class="w3-col s3">
+      <a href="inloggen.php" class="w3-button w3-block">Inloggen</a>
+    </div>
+  </div>
+</div>
+
+<!-- Content -->
+<div class="w3-content" style="max-width:1100px;margin-top:80px;margin-bottom:80px">
+  <div class="w3-panel">
+    <h1><b>Voorbeeldheader</b></h1>
+		<?php
+			$sql = "SELECT * FROM voorbeeldtable";
+			$result = $conn->query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				// output data of each row
+		?>
+			<table>
+					<tr>
+						<td><b>TEST1<b></td>
+						<td><b>TEST2<b></td>
+						<td><b>TEST3<b></td>
+						<td><b>Toevoegen<b></td>
+					<tr>
+						<?php
+							while($row = $result->fetch_assoc()) {
+						?> <tr><td> <?php echo $row["test1"]. "<br>"; ?> </td><?php
+						?> <td> <?php echo $row["test2"]. "<br>"; ?> </td><?php
+						?> <td> <?php echo $row["test3"]. "<br>"; ?> </td></tr><?php
+							}
+						}
+						?>
+	</div>
+</div>
+
+
+
+</body>
+</html>
+<?php
+$conn->close();
+?>
